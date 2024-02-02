10ecaeadb30913c5185b06eb4b0abb984385bf33
lenguyenchinguyen <145195459+lenguyenchinguyen@users.noreply.github.com>
2024-02-02 12:49:31 +0700

    Lab 7

---
+++ app.js
@@ -0,0 +1,10 @@
+const fullname = 'Lê Nguyễn Chí Nguyên';
+
+const person = {first: fullname};
+
+console.log(person);
+
+const SayHello = (name) =>{
+    console.log(`Hello, ${name}`);
+};
+SayHello(name);
